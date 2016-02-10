8d7072ed09906928f701e156a5099a1ae46e167d
Ciro Artigot <ciro.artigot@gmail.com>
2016-02-10 22:04:24 +0100

    1.2.0

--- app.js
+++ app.js
@@ -1,16 +1,5 @@
 
 var express = require('express');
-var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/ababooldb');
-
-var db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error aqui:'));
-db.once('open', function() {
-  // we're connected!
-  console.log('Connected to Mongo DB');
-});
-
-
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
@@ -18,19 +7,27 @@ var cookieParser = require('cookie-parser');
 var uuid = require('node-uuid');
 var bodyParser = require('body-parser');
 var session = require('express-session');
+
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/ababooldb');
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error'));
+db.once('open', function() {
+  console.log('Connected to Mongo DB');
+});
+
 var models = require('./routes/model/comment')(app, mongoose);
+
 var routes = require('./routes/index');
 var parseurl = require('parseurl');
 //var users = require('./routes/users');
 
 //the express app
 var app = express();
-
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
-
-// uncomment after placing your favicon in /public
 app.use(favicon(path.join(__dirname, 'public/images', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
@@ -38,7 +35,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 app.use(session({
-  secret: 'keyboard cat',
+  secret: 'Ababoll secret key',
   resave: false,
   saveUninitialized: true
 }));
@@ -49,7 +46,6 @@ app.use(function (req, res, next) {
       var uid = uuid.v1();
       token = req.session.token = uid;
     }
-    console.log('pathname: ' + parseurl(req).pathname);
     next()
 });
 
@@ -71,6 +67,5 @@ app.use(function(err, req, res, next) {
     error: {}
   });
 });
-
 
 module.exports = app;
